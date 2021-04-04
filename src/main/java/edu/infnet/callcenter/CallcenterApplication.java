ee9a87052d952d7d650760bc5453267168cc68ba
oliv4 <oliva.git@hotmail.com>
2021-04-04 16:10:19 -0300

    fix: application typo

--- src/main/java/edu/infnet/callcenter/CallcenterApplication.java
+++ src/main/java/edu/infnet/callcenter/CallcenterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CallcenterApplication {
+public class CallCenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CallcenterApplication.class, args);
+		SpringApplication.run(CallCenterApplication.class, args);
 	}
 
 }
