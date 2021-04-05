ab3dbad990f07bce44509eb2505f99293694fa8a
oliv4 <oliva.git@hotmail.com>
2021-04-05 20:52:30 -0300

    feat: add ticket many to one relation

--- src/main/java/edu/infnet/callcenter/dto/TicketDTO.java
+++ src/main/java/edu/infnet/callcenter/dto/TicketDTO.java
@@ -1,85 +1,138 @@
 package edu.infnet.callcenter.dto;
 
+import java.sql.Timestamp;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tickets")
 public class TicketDTO {
 
-		@Id
-		@GeneratedValue
-		private Long id;
-		private String serial_number;
-		private String product_name;
-		private String client_name;
-		private String client_telefone;
-		private String user_name;
-		private String status;
-		private String briefing;
-		private String error_code;
-		private String detail;
-		private Boolean finished;
-		public TicketDTO(){}
-		
-		
-		public Long getId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		public TicketDTO(String serial_number, String product_name, String client_name, String client_telefone,
-				String user_name, String status, String briefing, String error_code, String detail, Boolean finished) {
-			super();
-			this.serial_number = serial_number;
-			this.product_name = product_name;
-			this.client_name = client_name;
-			this.client_telefone = client_telefone;
-			this.user_name = user_name;
-			this.status = status;
-			this.briefing = briefing;
-			this.error_code = error_code;
-			this.detail = detail;
-			this.finished = finished;
-		}
+	@Id
+	@GeneratedValue
+	private Long id;
+	private Long user_id;
+	private Long serial_number;
+	private String status;
+	private String client_name;
+	private String briefing;
+	private String error_code;
+	private String description;
+	private Boolean finished;
+	private Timestamp created_at;
+	private Timestamp updated_at;
 
+	public TicketDTO() {
 
-		public String getSerial_number() {
-			return serial_number;
-		}
-		public String getProduct_name() {
-			return product_name;
-		}
-		public String getClient_name() {
-			return client_name;
-		}
-		public String getStatus() {
-			return status;
-		}
-		public String getBriefing() {
-			return briefing;
-		}
-		public String getError_code() {
-			return error_code;
-		}
-		public String getDetail() {
-			return detail;
-		}
-		public Boolean getFinished() {
-			return finished;
-		}
+	}
 
+	public TicketDTO(Long serial_number, String status, String client_name, String briefing, String error_code,
+			String description, Boolean finished, Timestamp created_at, Timestamp updated_at) {
+		this.serial_number = serial_number;
+		this.status = status;
+		this.client_name = client_name;
+		this.briefing = briefing;
+		this.error_code = error_code;
+		this.description = description;
+		this.finished = finished;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
 
-		public String getClient_telefone() {
-			return client_telefone;
-		}
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public String getUser_name() {
-			return user_name;
-		}
-		
+	public Long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+
+	public Long getSerial_number() {
+		return serial_number;
+	}
+
+	public void setSerial_number(Long serial_number) {
+		this.serial_number = serial_number;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getClient_name() {
+		return client_name;
+	}
+
+	public void setClient_name(String client_name) {
+		this.client_name = client_name;
+	}
+
+	public String getBriefing() {
+		return briefing;
+	}
+
+	public void setBriefing(String briefing) {
+		this.briefing = briefing;
+	}
+
+	public String getError_code() {
+		return error_code;
+	}
+
+	public void setError_code(String error_code) {
+		this.error_code = error_code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Boolean finished) {
+		this.finished = finished;
+	}
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+	public Timestamp getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Timestamp updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	@ManyToOne(optional=true, fetch=FetchType.LAZY)
+	@JoinColumn(name="user_id", referencedColumnName="id", insertable=false, updatable=false)
+	private UserDTO users;
 }
