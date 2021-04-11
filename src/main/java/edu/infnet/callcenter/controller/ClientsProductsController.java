721fc9df31bad49c1d48692749766e229060fd32
oliv4 <oliva.git@hotmail.com>
2021-04-11 18:18:21 -0300

    clientsproductscontroller in comment blocks

--- src/main/java/edu/infnet/callcenter/controller/ClientsProductsController.java
+++ src/main/java/edu/infnet/callcenter/controller/ClientsProductsController.java
@@ -15,10 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.infnet.callcenter.dto.ClientDTO;
-import edu.infnet.callcenter.dto.ClientsProductsDTO;
+import edu.infnet.callcenter.dto.ClientProductDTO;
 import edu.infnet.callcenter.dto.ProductDTO;
 import edu.infnet.callcenter.services.ClientService;
-import edu.infnet.callcenter.services.ClientsProductsService;
 import edu.infnet.callcenter.services.ProductService;
 
 @RestController 
@@ -29,34 +28,31 @@ public class ClientsProductsController {
 	private ClientService cs;
 	@Autowired 
 	private ProductService ps;
-	@Autowired
-	private ClientsProductsService cps;
-	
 
-	@GetMapping("cpf/{cpf}")
-	public List<ProductDTO> getProductsByCPF(@PathVariable String cpf){
-		ClientDTO client = cs.getByCpf(cpf).get(0);
-		List<ProductDTO> ProductList = new ArrayList();
-		for (ClientsProductsDTO clientProduct : cps.getAllByClient(client)){
-			ProductList.add(clientProduct.getProduct());
-		}
+	// @GetMapping("/{cpf}")
+	// public List<ProductDTO> getProductsByCPF(@PathVariable String cpf){
+	// 	ClientDTO client = cs.getByCpf(cpf).get(0);
+	// 	List<ProductDTO> ProductList = new ArrayList();
+	// 	for (ClientProductDTO clientProduct : cps.getAllByClient(client)){
+	// 		ProductList.add(clientProduct.getProduct());
+	// 	}
 		
-		return ProductList;
-	}
+	// 	return ProductList;
+	// }
 
 	
-	@PatchMapping("/{id_client}/{id_product}")
-	public ClientsProductsDTO addProduct (@PathVariable Long id_client, @PathVariable Long id_product) {
-		if(!ps.exists(id_product) || !cs.exists(id_client) ) return null;
+	// @PatchMapping("/{id_client}/{id_product}")
+	// public ClientProductDTO addProduct (@PathVariable Long id_client, @PathVariable Long id_product) {
+	// 	if(!ps.exists(id_product) || !cs.exists(id_client) ) return null;
 		
-		ClientsProductsDTO clients_products = new ClientsProductsDTO(cs.getById(id_client).get(),ps.getById(id_product).get() );
+	// 	ClientProductDTO clients_products = new ClientProductDTO(cs.getById(id_client).get(),ps.getById(id_product).get() );
 
-		return cps.add(clients_products);
-	}
+	// 	return cps.add(clients_products);
+	// }
 	
-	@DeleteMapping("/{id}")
-	public Optional<ClientsProductsDTO> removeProduct(@PathVariable Long id) {
-		if(!cps.exists(id)) return null;	
-		return cps.deleteById(id);
-	}
+	// @DeleteMapping("/{id}")
+	// public Optional<ClientProductDTO> removeProduct(@PathVariable Long id) {
+	// 	if(!cps.exists(id)) return null;	
+	// 	return cps.deleteById(id);
+	// }
 }
