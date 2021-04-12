f52b2da8f47da1c03f25530bbf552fad233e0600
CRISTIAN TRUCCO <cristianvitortrucco@gmail.com>
2021-04-11 21:34:19 -0300

    implementação do Login

---
+++ src/main/java/edu/infnet/callcenter/controller/LoginController.java
@@ -0,0 +1,15 @@
+package edu.infnet.callcenter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
